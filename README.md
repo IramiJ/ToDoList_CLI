81fdcce6de967d98251cdc9db73f9b7121f70b5f
Colliname <yugiohduel3@gmail.com>
2025-08-14 13:40:37 +0200

    added all the basic features to the todo list

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ToDoList_CLI
 Small programming project to help me get started with programming again. 
+
+This is a command like based todo list, that works on a very simple level. It allows you to delete/add tasks and view them.
--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("hello world")
+todo_list = []
+
+def add(task):
+ todo_list.append(task)
+ print("task added")
+def delete(index):
+ try:
+  if int(index) <= len(todo_list):
+   todo_list.remove(todo_list[int(index) -1])
+   print("task deleted")
+  else:
+   print("delete failed!")
+ except:
+  print("invalid input")
+def view_tasks():
+ if todo_list == []:
+  print("No tasks to be done")
+ else:
+  for i, task in enumerate(todo_list, 1):
+   print(f"{i}. {task}")
+
+while True:
+ user_input = input("what action do you want to perform? (add,delete,view,quit)? ").lower()
+ if user_input == "add":
+  to_push = input("what do you want to add? ")
+  add(to_push)
+ elif user_input == "delete":
+  to_delete = input("which task do you wish to delete? ")
+  delete(to_delete)
+ elif user_input == "view":
+  view_tasks()
+ elif user_input == "quit":
+  break
+ else:
+  print("invalid action!! ")

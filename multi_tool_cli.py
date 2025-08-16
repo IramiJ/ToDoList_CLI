b3dc05370ab24ff9a59f38b278c6419447ee9cfa
Colliname <yugiohduel3@gmail.com>
2025-08-16 08:43:25 +0200

    made it so that every script is usable as a function, to merge it into one script

---
+++ multi_tool_cli.py
@@ -0,0 +1,22 @@
+from todo_list import run_todo
+from calculator import run_calculator
+from file_organizer import run_file_organizer
+
+while True:
+ print("\n=== Multi-Tool CLI ===")
+ print("1. To-do List")
+ print("2. File Organizer")
+ print("3. Calculator")
+ print("4. Quit")
+
+ inp = input("choose an option ")
+ if inp == "1":
+  run_todo()
+ elif inp == "2":
+  run_file_organizer()
+ elif inp == "3":
+  run_calculator()
+ elif inp == "4":
+  break
+ else: 
+  print("invalid option! please try again")
--- todo_list.py
+++ todo_list.py
@@ -1,43 +1,45 @@
-todo_list = []
+def run_todo():
 
-class task():
- def __init__(self, content, due):
-  self.content = content
-  self.due = due
+  todo_list = []
 
-def add(content, due):
- todo_list.append(task(content, due))
- print("task added")
+  class task():
+    def __init__(self, content, due):
+      self.content = content
+      self.due = due
 
-def delete(index):
- try:
-  if int(index) <= len(todo_list):
-   todo_list.remove(todo_list[int(index) -1])
-   print("task deleted")
-  else:
-   print("delete failed!")
- except:
-  print("invalid input")
+  def add(content, due):
+    todo_list.append(task(content, due))
+    print("task added")
 
-def view_tasks():
- if todo_list == []:
-  print("No tasks to be done")
- else:
-  for i, task in enumerate(todo_list, 1):
-   print(f"{i}. {task.content}, due at {task.due}")
+  def delete(index):
+    try:
+      if int(index) <= len(todo_list):
+        todo_list.remove(todo_list[int(index) -1])
+        print("task deleted")
+      else:
+        print("delete failed!")
+    except:
+      print("invalid input")
 
-while True:
- user_input = input("what action do you want to perform? (add,delete,view,quit)? ").lower()
- if user_input == "add":
-  content = input("what do you want to add? ")
-  due = input("when is the task due? ")
-  add(content, due)
- elif user_input == "delete":
-  to_delete = input("which task do you wish to delete? ")
-  delete(to_delete)
- elif user_input == "view":
-  view_tasks()
- elif user_input == "quit":
-  break
- else:
-  print("invalid action!! ")
+  def view_tasks():
+    if todo_list == []:
+     print("No tasks to be done")
+    else:
+      for i, task in enumerate(todo_list, 1):
+        print(f"{i}. {task.content}, due at {task.due}")
+
+  while True:
+    user_input = input("what action do you want to perform? (add,delete,view,quit)? ").lower()
+    if user_input == "add":
+      content = input("what do you want to add? ")
+      due = input("when is the task due? ")
+      add(content, due)
+    elif user_input == "delete":
+      to_delete = input("which task do you wish to delete? ")
+      delete(to_delete)
+    elif user_input == "view":
+      view_tasks()
+    elif user_input == "quit":
+      break
+    else:
+      print("invalid action!! ")
